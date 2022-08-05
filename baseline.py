d82ce9ec9624873a746603e6038b3ef43992e21b
Xiefeng69 <854438622@qq.com>
2022-08-05 12:20:17 +0800

    fix baseline

--- baseline.py
+++ baseline.py
@@ -1,3 +1,4 @@
+from multiprocessing import reduction
 import os
 import math
 import torch
@@ -35,8 +36,10 @@ class Dataset(object):
         self.anchor = torch.from_numpy(anchor)
         self.a1anchor = self.anchor[:,0].numpy()
         self.a2anchor = self.anchor[:,1].numpy()
+        self.negative = np.random.randint(low=0, high=1034, size=(1034))
+        self.negative.tolist()
     def __getitem__(self, index):
-        return self.a1anchor[index], self.a2anchor[index]
+        return self.a1anchor[index], self.a2anchor[index], self.negative[index]
     def __len__(self):
         return len(self.a1anchor)
 
@@ -69,15 +72,15 @@ class GraphConvLayer(nn.Module):
 class Model(nn.Module):
     def __init__(self, A1, A2, embedding_dim):
         super().__init__()
-        self.l = 2
+        self.l = 3
         self.embedding_dim = embedding_dim
         self.num_sr = A1.shape[0]
         self.num_tg = A2.shape[0]
         self.adj1 = A1
         self.adj2 = A2
-        self.dropout = nn.Dropout(0.5)
-        self.embedding_sr = nn.Parameter(torch.FloatTensor(self.num_sr, self.embedding_dim), requires_grad=True)
-        self.embedding_tg = nn.Parameter(torch.FloatTensor(self.num_tg, self.embedding_dim), requires_grad=True)
+        self.dropout = nn.Dropout(0.2)
+        self.embedding1 = nn.Parameter(torch.FloatTensor(self.num_sr, self.embedding_dim), requires_grad=True)
+        self.embedding2 = nn.Parameter(torch.FloatTensor(self.num_tg, self.embedding_dim), requires_grad=True)
         # self.embedding_sr = nn.Embedding(self.num_sr, self.embedding_dim, _weight=torch.zeros((self.num_sr, self.embedding_dim), dtype=torch.float))
         # self.embedding_tg = nn.Embedding(self.num_tg, self.embedding_dim, _weight=torch.zeros((self.num_tg, self.embedding_dim), dtype=torch.float))
         self.gcnblocks = nn.ModuleList([GraphConvLayer(in_features=self.embedding_dim, out_features=self.embedding_dim) for i in range(self.l)])
@@ -92,8 +95,8 @@ class Model(nn.Module):
         self.embedding_tg.weight.data = F.normalize(self.embedding_tg.weight, dim=-1, p=2)
     
     def forward(self):
-        a1_embeddings = self.embedding_sr
-        a2_embeddings = self.embedding_tg
+        a1_embeddings = self.embedding1
+        a2_embeddings = self.embedding2
         for layer in self.gcnblocks:
             a1_embeddings = layer(a1_embeddings, self.adj1)
             a1_embeddings = self.dropout(a1_embeddings)
@@ -101,7 +104,26 @@ class Model(nn.Module):
             a2_embeddings = self.dropout(a2_embeddings)
         return a1_embeddings, a2_embeddings
 
-def evaluate(Embedding1, Embedding2, sim_measure="euclidean"):
+def compute_loss(criterion, a1_embedding, a2_embedding, a2_neg_embedding, neg_margin=3, neg_param=0.5, only_pos=False):
+    # positive pair loss computation
+    pos_loss = torch.abs(a1_embedding - a2_embedding)
+    pos_loss = torch.sum(pos_loss, dim=1)
+    pos_loss = torch.sum(pos_loss)
+    pos_loss = Variable(pos_loss, requires_grad=True)
+    # negative pair loss computation
+    neg_loss = torch.abs(a1_embedding - a2_neg_embedding)
+    neg_loss = torch.sum(neg_loss, dim=1)
+    neg_loss = F.relu(neg_margin - neg_loss)
+    neg_loss = torch.sum(neg_loss)
+    neg_loss = Variable(neg_loss, requires_grad=True)
+    # return pos_loss + neg_param * neg_loss
+    return pos_loss + neg_param * neg_loss
+
+def evaluate(Embedding1, Embedding2, sim_measure="cosine"):
+    Embedding1 = F.normalize(Embedding1, dim=1)
+    Embedding2 = F.normalize(Embedding2, dim=1)
+    Embedding1 = Embedding1.detach().numpy()
+    Embedding2 = Embedding2.detach().numpy()
     if sim_measure == "cosine":
         similarity_matrix = cosine_similarity(Embedding1, Embedding2)
     else:
@@ -114,14 +136,14 @@ def evaluate(Embedding1, Embedding2, sim_measure="euclidean"):
         alignment_hit1.append(idx)
         idxs = heapq.nlargest(10, range(len(line)), line.take)
         alignment_hit5.append(idxs)
-    return alignment_hit1, alignment_hit5
+    return similarity_matrix, alignment_hit1, alignment_hit5
 
 ##############
 # building block
-epoch = 30
+epoch = 10
 embedding_dim = 1000
 batchsize = 16
-learning_rate = 0.0001
+learning_rate = 0.01
 weight_decay = 1e-5
 A1, A2, anchor = load_data()
 dataset = Dataset(anchor)
@@ -135,11 +157,12 @@ for e in range(epoch):
     for i, data in enumerate(train_loader):
         model.train()
         optimizer.zero_grad()
-        a1_align, a2_align = data
+        a1_align, a2_align, a2_neg_align = data
         E1, E2 = model()
         a1_embedding = E1[a1_align.long()]
-        a2_embedding = E1[a2_align.long()]
-        loss = criterion(a1_embedding, a2_embedding)
+        a2_embedding = E2[a2_align.long()]
+        a2_neg_embedding = E2[a2_neg_align.long()]
+        loss = compute_loss(criterion, a1_embedding, a2_embedding, a2_neg_embedding)
         loss.backward()
         optimizer.step()
     print(f"epoch: {e+1}, loss: {loss}\n")
@@ -147,9 +170,8 @@ for e in range(epoch):
 # test
 model.eval()
 E1, E2 = model()
-E1 = E1.detach().numpy()
-E2 = E2.detach().numpy()
-alignment_hit1, alignment_hit5 = evaluate(Embedding1=E1, Embedding2=E2)
+similarity_matrix, alignment_hit1, alignment_hit5 = evaluate(Embedding1=E1, Embedding2=E2)
+print(similarity_matrix)
 
 ground_truth = np.loadtxt('ground_truth.txt', delimiter=' ')
 hit_1 = 0
